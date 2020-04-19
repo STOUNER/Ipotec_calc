8a249a6843e238a46ae13c8663f825e21ae687f9
STOUNER <ShraikNew@gmail.com>
2020-04-20 01:39:36 +0300

    Изменения переменных, новая логика

--- src/Calculated.java
+++ src/Calculated.java
@@ -1,14 +1,21 @@
  public class Calculated {
   static int MonthInYear = 12;
-  static int PaymantPeriod;
-  static int CurrentBalance;
-  static int StartingBalance;
+  static int PaymentPeriod;
+  static double CurrentBalance;
+  static double StartingBalance;
   static int InterestRate;
+  static int MonthPercent = InterestRate / MonthInYear;
 
-  public Calculated(int StartingBalance, int years, int interestRate) {
-   this.StartingBalance = StartingBalance;
-   this.PaymantPeriod = years * MonthInYear;
+  public Calculated(int loanAmount, int years, int interestRate) {
+   this.StartingBalance = loanAmount;
+   this.PaymentPeriod = years * MonthInYear;
    this.InterestRate = interestRate;
   }
-
+  public static double Amount(){
+      double Interest;
+      CurrentBalance = StartingBalance;
+      Interest = (CurrentBalance * MonthPercent) / Math.pow((1 -(1+MonthPercent)),(MonthInYear*-1));
+      StartingBalance =- Interest;
+      return Interest;
+ }
  }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Calculated Test_1 = new Calculated(500,10,10);
-        for (int i = 0; i < Calculated.PaymantPeriod; i++){
-            System.out.println();
+        Calculated Test_1 = new Calculated(500,1,10);
+        for (int i = 0; i < Calculated.PaymentPeriod; i++){
+            System.out.println(Calculated.Amount());
         }
     }
 }
