ab8c8a91b1b1f763f4ee67b2fb1f77488c2a5b9b
STOUNER <ShraikNew@gmail.com>
2020-04-22 03:49:04 +0300

    Множественные доработки.

--- src/Calculated.java
+++ src/Calculated.java
@@ -1,21 +1,35 @@
  public class Calculated {
   static int MonthInYear = 12;
   static int PaymentPeriod;
-  static double CurrentBalance;
   static double StartingBalance;
+  static double EndingBalance;
   static int InterestRate;
-  static int MonthPercent = InterestRate / MonthInYear;
+  static double MonthPercent;
+  static double BankReward;
+  static double TotalPayment;
+  static double loanAmount;
 
   public Calculated(int loanAmount, int years, int interestRate) {
+   this.loanAmount = loanAmount;
    this.StartingBalance = loanAmount;
    this.PaymentPeriod = years * MonthInYear;
    this.InterestRate = interestRate;
+   this.MonthPercent = (double)InterestRate / 100 / 12;
+   this.EndingBalance = loanAmount;
   }
-  public static double Amount(){
+  public static double Paid(){
+     double Payment;
+     Payment = (loanAmount * MonthPercent) / (1-(Math.pow((1+MonthPercent),(-PaymentPeriod))));
+     BankReward += Payment;
+     return Payment;
+ }
+     public static double Interest(){
       double Interest;
-      CurrentBalance = StartingBalance;
-      Interest = (CurrentBalance * MonthPercent) / Math.pow((1 -(1+MonthPercent)),(MonthInYear*-1));
-      StartingBalance =- Interest;
+      StartingBalance = EndingBalance;
+      Interest = StartingBalance * MonthPercent;
+      EndingBalance -= Interest;
+      TotalPayment += Interest;
       return Interest;
- }
- }
+     }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Calculated Test_1 = new Calculated(500,1,10);
+        Calculated Test_1 = new Calculated(100000,30,6);
         for (int i = 0; i < Calculated.PaymentPeriod; i++){
-            System.out.println(Calculated.Amount());
+            System.out.println(Calculated.Paid());
+            System.out.println(Calculated.Interest());;
         }
+        System.out.println(Calculated.BankReward);
+        System.out.println(Calculated.TotalPayment);
     }
 }
