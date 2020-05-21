375a2819d922142dece8d9ffcb503096300eea49
STOUNER <ShraikNew@gmail.com>
2020-05-22 00:22:42 +0300

    Множественные доработки.

--- src/Calculated.java
+++ src/Calculated.java
@@ -5,14 +5,25 @@ public class Calculated {
     double EndingBalance;
     int InterestRate;
     double MonthPercent;
-    double BankReward;
+    double TotalInterest;
     double TotalPayment;
     double loanAmount;
     double Interest;
     double Payment;
+    double Principal;
+    int Month = 1;
 
     public Calculated(int loanAmount, int years, int interestRate) {
         this.loanAmount = loanAmount;
+        this.StartingBalance = loanAmount;
+        this.PaymentPeriod = years * MonthInYear;
+        this.InterestRate = interestRate;
+        this.MonthPercent = (double) InterestRate / 100 / 12;
+        this.EndingBalance = loanAmount;
+    }
+
+    public Calculated(int loanAmount, int years, int interestRate,double ExtraPayment) {
+        this.loanAmount = loanAmount - ExtraPayment;
         this.StartingBalance = loanAmount;
         this.PaymentPeriod = years * MonthInYear;
         this.InterestRate = interestRate;
@@ -24,22 +35,28 @@ public class Calculated {
         double Payment;
         Payment = (loanAmount * MonthPercent) / (1 - (Math.pow((1 + MonthPercent), (-PaymentPeriod))));
         this.Payment = Payment;
+        TotalPayment += Payment;
         return Payment;
     }
 
     public double Interest() {
         StartingBalance = EndingBalance;
         Interest = StartingBalance * MonthPercent;
-        TotalPayment += Interest;
         return Interest;
+
     }
 
     public double Principal() {
         double Principal;
         Principal = Payment - Interest;
         EndingBalance -= Principal;
+        this.Principal = Principal;
         return Principal;
+    }
 
+    public double TotalInterest() {
+        TotalInterest += (Payment - Principal);
+        return TotalInterest;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Calculated Test_1 = new Calculated(100000,30,6);
-        for (int i = 0; i < Test_1.PaymentPeriod; i++){
+        Calculated Test_1 = new Calculated(100000, 1, 6, 5000);
+        for (int i = 0; i < Test_1.PaymentPeriod; i++) {
+            System.out.println("Месяц: " + (Test_1.Month + i));
             System.out.println("Платёж: " + Test_1.YouPaid());
-            System.out.println("Процент банку: " + Test_1.Interest());;
+            System.out.println("Процент банку: " + Test_1.Interest());
             System.out.println("Тело кредита: " + Test_1.Principal());
+            System.out.println("Сумма переплаты " + Test_1.TotalInterest());
             System.out.println(' ');
         }
-        System.out.println(Test_1.BankReward);
+        System.out.println(Test_1.TotalInterest);
         System.out.println(Test_1.TotalPayment);
     }
 }
