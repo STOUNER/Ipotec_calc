a2810b3bc41c5049928b308b30efb1c0895237f4
STOUNER <ShraikNew@gmail.com>
2020-05-21 22:27:57 +0300

    Множественные доработки.

--- src/Calculated.java
+++ src/Calculated.java
@@ -1,35 +1,45 @@
- public class Calculated {
-  static int MonthInYear = 12;
-  static int PaymentPeriod;
-  static double StartingBalance;
-  static double EndingBalance;
-  static int InterestRate;
-  static double MonthPercent;
-  static double BankReward;
-  static double TotalPayment;
-  static double loanAmount;
+public class Calculated {
+    int MonthInYear = 12;
+    int PaymentPeriod;
+    double StartingBalance;
+    double EndingBalance;
+    int InterestRate;
+    double MonthPercent;
+    double BankReward;
+    double TotalPayment;
+    double loanAmount;
+    double Interest;
+    double Payment;
 
-  public Calculated(int loanAmount, int years, int interestRate) {
-   this.loanAmount = loanAmount;
-   this.StartingBalance = loanAmount;
-   this.PaymentPeriod = years * MonthInYear;
-   this.InterestRate = interestRate;
-   this.MonthPercent = (double)InterestRate / 100 / 12;
-   this.EndingBalance = loanAmount;
-  }
-  public static double Paid(){
-     double Payment;
-     Payment = (loanAmount * MonthPercent) / (1-(Math.pow((1+MonthPercent),(-PaymentPeriod))));
-     BankReward += Payment;
-     return Payment;
- }
-     public static double Interest(){
-      double Interest;
-      StartingBalance = EndingBalance;
-      Interest = StartingBalance * MonthPercent;
-      EndingBalance -= Interest;
-      TotalPayment += Interest;
-      return Interest;
-     }
+    public Calculated(int loanAmount, int years, int interestRate) {
+        this.loanAmount = loanAmount;
+        this.StartingBalance = loanAmount;
+        this.PaymentPeriod = years * MonthInYear;
+        this.InterestRate = interestRate;
+        this.MonthPercent = (double) InterestRate / 100 / 12;
+        this.EndingBalance = loanAmount;
+    }
+
+    public double YouPaid() {
+        double Payment;
+        Payment = (loanAmount * MonthPercent) / (1 - (Math.pow((1 + MonthPercent), (-PaymentPeriod))));
+        this.Payment = Payment;
+        return Payment;
+    }
+
+    public double Interest() {
+        StartingBalance = EndingBalance;
+        Interest = StartingBalance * MonthPercent;
+        TotalPayment += Interest;
+        return Interest;
+    }
+
+    public double Principal() {
+        double Principal;
+        Principal = Payment - Interest;
+        EndingBalance -= Principal;
+        return Principal;
+
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Calculated Test_1 = new Calculated(100000,30,6);
-        for (int i = 0; i < Calculated.PaymentPeriod; i++){
-            System.out.println(Calculated.Paid());
-            System.out.println(Calculated.Interest());;
+        for (int i = 0; i < Test_1.PaymentPeriod; i++){
+            System.out.println("Платёж: " + Test_1.YouPaid());
+            System.out.println("Процент банку: " + Test_1.Interest());;
+            System.out.println("Тело кредита: " + Test_1.Principal());
+            System.out.println(' ');
         }
-        System.out.println(Calculated.BankReward);
-        System.out.println(Calculated.TotalPayment);
+        System.out.println(Test_1.BankReward);
+        System.out.println(Test_1.TotalPayment);
     }
 }
